a8ffe3e5525157841e806b5d70fa5dbf2452107d
Rolando-Barbella <rolandobarbella@gmail.com>
2020-03-09 17:53:32 +0800

    Added lesson 5

--- src/App.js
+++ src/App.js
@@ -7,6 +7,15 @@ function App() {
   const [countries, setCountries] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
   const [isError, setIsError] = useState(false);
+  const [isEditMedals, setIsEditMedals] = useState({ 
+    showForm: false, 
+    gold: null, 
+    silver: null, 
+    bronze: null, 
+    flag: null, 
+    countryName: null 
+  });
+  const [onChangeMedal, setOnChangeMedal] = useState({ gold: '', silver: '', bronze: ''});
 
   useEffect(() => {
     const fetchCountries = async() => {
@@ -25,6 +34,20 @@ function App() {
     }
     fetchCountries();
   }, []);
+
+  const editMedals = (country) => {
+    const { medals: [{ gold, silver, bronze }], flag, name} = country;
+
+    setIsEditMedals({ showForm: true, gold, silver, bronze, flag, countryName: name });
+    setOnChangeMedal({ gold, silver, bronze });
+  }
+
+  const handleInputChange = (event, keyName) => {
+    event.persist();
+    setOnChangeMedal(() => {
+      return {[keyName]: event.target.value}
+    })
+  };
 
   if(isError) {
     return (
@@ -46,14 +69,14 @@ function App() {
     <div className="App">
       <section className="App-container">
         <h3>Cuadro de medallas</h3>
-        <table>
+        <table width="800" border="1" cellPadding="1" cellSpacing="1">
           <tbody>
             <tr>
               <th></th>
               <th></th>
-              <th>Oro | <br/></th>
-              <th>Plata | <br/></th>
-              <th>Bronce | <br/></th>
+              <th>Oro <br/></th>
+              <th>Plata <br/></th>
+              <th>Bronce <br/></th>
               <th>Total</th>
             </tr>
           </tbody>
@@ -64,7 +87,12 @@ function App() {
                 <tbody key={country.id}>
                   <tr>
                     <th>{country.flag}</th>
-                    <th>{country.name}</th>
+                    <th 
+                      onClick={() => editMedals(country)}
+                      className="edit-medals"
+                    >
+                      {country.name}
+                    </th>
                     <th>{gold}</th>
                     <th>{silver}</th>
                     <th>{bronze}</th>
@@ -75,6 +103,51 @@ function App() {
             })
           }
         </table>
+        <div className="medal-form-container">
+          {
+            isEditMedals.showForm &&
+            <>
+              <div className="country-selected-wrapper">
+                <span>{isEditMedals.flag}</span>
+                <p>{isEditMedals.countryName}</p>
+              </div>
+              <form action="" className="medal-form">
+                <div className="update-container">
+                  <label htmlFor="">Oro:</label>
+                  <input 
+                    type="text" 
+                    value={onChangeMedal.gold} 
+                    className="medal-input"
+                    onChange={(event) => handleInputChange(event, 'gold')}
+                  />
+                </div>
+                <div className="update-container">
+                  <label htmlFor="">Plata:</label>
+                  <input 
+                    type="text" 
+                    value={onChangeMedal.silver} 
+                    className="medal-input"
+                    onChange={(event) => handleInputChange(event, 'silver')}
+                  />
+                </div>
+                <div className="update-container">
+                  <label htmlFor="">Bronce:</label>
+                  <input 
+                    type="text" 
+                    value={onChangeMedal.bronze} 
+                    className="medal-input"
+                    onChange={(event) => handleInputChange(event, 'bronze')}
+                  />
+                </div>
+                <div className="update-container">
+                  <button className="update-btn">
+                    Actualizar
+                  </button>
+                </div>
+              </form>
+            </>
+          }
+        </div>
       </section>
     </div>
   );
