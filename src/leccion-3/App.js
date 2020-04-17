41b1325e7ef473f769b6851d88340a22a26ce31a
Rolando-Barbella <rolandobarbella@gmail.com>
2020-04-17 15:19:56 +0530

    Added lesson 3 folder

---
+++ src/leccion-3/App.js
@@ -0,0 +1,62 @@
+import React, { useEffect, useState } from 'react';
+import './App.css';
+
+const URL = 'http://localhost:4000/countries';
+
+function App() {
+  const [countries, setCountries] =  useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchCountries = async() => {
+      const response = await fetch(URL);
+      const countries = await response.json();
+
+      setCountries(countries);
+      setIsLoading(false)
+    }
+    fetchCountries();
+  },[]);
+
+  if(isLoading) {
+    return <p>...Loading</p>
+  }
+
+  return (
+    <div className="App">
+      <section className="App-container">
+        <table>
+          <tbody>
+            <tr>
+              <th></th> 
+              <th></th> 
+              <th> Oro | <br/></th> 
+              <th> Plata | <br/></th> 
+              <th> Bronce | <br/></th> 
+              <th> Total <br/></th>
+            </tr>
+          </tbody>
+          {
+            countries.map(country => {
+              const { medals:[ { gold, silver, bronze } ]} = country;
+              return (
+                <tbody key={country.id}>
+                  <tr>
+                    <th>{country.flag}</th>
+                    <th>&nbsp;{country.name}</th>
+                    <th>{gold}</th>
+                    <th>{silver}</th>
+                    <th>{bronze}</th>
+                    <th>{gold + silver + bronze}</th>
+                  </tr>
+                </tbody>
+              )
+            })
+          }
+        </table>
+      </section>
+    </div>
+  );
+}
+
+export default App;
